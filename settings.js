e631d859a70405f7ada40f01bfb7f998ef06b8f5
Dthimira679 <dthimira38@gmail.com>
2025-05-19 15:10:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Zrl31L5K#zvU15NUP9ILJ-gZ6SmFLvaNT-oJUKud-AVtxAF8QYDM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=J2tzhIAC#MrPHlIx6GvByF2i2nzFd2QTvY3oWuAQVWtPM83bkZTU' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
